0b4e64d26baa23d5313e6f314b600d3d75a8a980
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-02-19 17:27:49 -0800

    DB schemad and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+t1 = {
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  }
+  
+t2 = {
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  }
+
+Todo.create(t1)
+Todo.create(t2)
